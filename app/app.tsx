62d0436b690b77479563d27cb9a406bf02e0c6da
LukassF <szkulkerolf@gmail.com>
2024-08-20 17:08:30 +0200

    add link to aptos

--- app/app.tsx
+++ app/app.tsx
@@ -1,10 +1,11 @@
-'use client'
-import { Loader } from '@react-three/drei'
-import { Toaster } from 'sonner'
-import Background from './components/Background'
-import StickyHeader from './components/StickyHeader'
-import Torus from './components/Torus/App'
-import Socials from './components/Socials'
+"use client";
+import { Loader } from "@react-three/drei";
+import { Toaster } from "sonner";
+import Background from "./components/Background";
+import StickyHeader from "./components/StickyHeader";
+import Torus from "./components/Torus/App";
+import Socials from "./components/Socials";
+import AptosLink from "./components/AptosLink";
 
 export default function Home() {
   return (
@@ -12,9 +13,10 @@ export default function Home() {
       <Background />
       <StickyHeader />
       <Torus />
-      <Toaster position='bottom-left' richColors />
+      <Toaster position="bottom-left" richColors />
       <Socials />
+      <AptosLink />
       <Loader />
     </>
-  )
+  );
 }
--- app/components/StickyHeader.tsx
+++ app/components/StickyHeader.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react'
-import { toast } from 'sonner'
-import { getAdapter } from '../misc/adapter'
-import ActionStarryButton from './ActionStarryButton'
-import StarryButton from './StarryButton'
-import { AccountInfo, UserResponseStatus } from '@aptos-labs/wallet-standard'
-import { getMovement } from '../misc/movement'
-import { Network } from '@aptos-labs/ts-sdk'
+import React, { useEffect } from "react";
+import { toast } from "sonner";
+import { getAdapter } from "../misc/adapter";
+import ActionStarryButton from "./ActionStarryButton";
+import StarryButton from "./StarryButton";
+import { AccountInfo, UserResponseStatus } from "@aptos-labs/wallet-standard";
+import { getMovement } from "../misc/movement";
+import { Network } from "@aptos-labs/ts-sdk";
 
 const StickyHeader: React.FC = () => {
-  const [userAccount, setUserAccount] = React.useState<AccountInfo>()
+  const [userAccount, setUserAccount] = React.useState<AccountInfo>();
 
   return (
-    <header className='fixed top-0 left-0 w-full bg-opacity-50  p-6 z-10'>
-      <div className='flex items-center justify-between'>
+    <header className="fixed top-0 left-0 w-full bg-opacity-50  p-6 z-10">
+      <div className="flex items-center justify-between">
         <div>
           {/* <Image
             style={{ width: '200px', cursor: 'pointer' }}
@@ -25,58 +25,63 @@ const StickyHeader: React.FC = () => {
             }}
           /> */}
         </div>
-        <div className='flex flex-col space-y-4'>
+        <div className="flex flex-col space-y-4">
           <StarryButton
             connected={userAccount?.address !== undefined}
             onConnect={async () => {
-              const adapter = await getAdapter()
+              const adapter = await getAdapter();
               try {
                 const response = await adapter.connect(undefined, {
                   chainId: 27,
                   name: Network.CUSTOM,
-                  url: 'https://aptos.testnet.suzuka.movementlabs.xyz/v1',
-                })
+                  url: "https://aptos.testnet.suzuka.movementlabs.xyz/v1",
+                });
                 if (response.status === UserResponseStatus.APPROVED) {
-                  setUserAccount(response.args)
-                  toast.success('Wallet connected!')
+                  setUserAccount(response.args);
+                  toast.success("Wallet connected!");
                 } else {
-                  toast.error('User rejected connection')
-                  return
+                  toast.error("User rejected connection");
+                  return;
                 }
               } catch (error) {
-                toast.error('Wallet connection failed!')
+                toast.error("Wallet connection failed!");
                 // If error, disconnect ignore error
-                await adapter.disconnect().catch(() => {})
-                return
+                await adapter.disconnect().catch(() => {});
+                return;
               }
               try {
                 // Check chainId
-                const chainId = await adapter.network()
+                const chainId = await adapter.network();
                 if (chainId.chainId !== 27) {
                   // If chainId is different than 4 (movement devnet) change it
                   const changeNetworkResponse = await adapter.changeNetwork({
                     chainId: 27,
                     name: Network.CUSTOM,
-                    url: 'https://aptos.testnet.suzuka.movementlabs.xyz/v1',
-                  })
-                  if (changeNetworkResponse.status === UserResponseStatus.APPROVED) {
-                    toast.success('Network changed!')
+                    url: "https://aptos.testnet.suzuka.movementlabs.xyz/v1",
+                  });
+                  if (
+                    changeNetworkResponse.status === UserResponseStatus.APPROVED
+                  ) {
+                    toast.success("Network changed!");
                   } else {
-                    toast.error('User rejected network change')
-                    return
+                    toast.error("User rejected network change");
+                    return;
                   }
                 }
               } catch (error) {
-                console.log(error)
+                console.log(error);
               }
             }}
             onDisconnect={async () => {
               try {
-                const adapter = await getAdapter()
-                await adapter.disconnect()
-                setUserAccount(undefined)
+                console.log("start");
+                const adapter = await getAdapter();
+                console.log(adapter);
+                await adapter.disconnect();
+                console.log("done");
+                setUserAccount(undefined);
               } catch (error) {
-                console.log(error)
+                console.log(error);
               }
             }}
             publicKey={userAccount?.address.toString()}
@@ -85,46 +90,48 @@ const StickyHeader: React.FC = () => {
             <>
               <ActionStarryButton
                 onClick={async () => {
-                  const signingToast = toast.info('Signing Transaction...')
+                  const signingToast = toast.info("Signing Transaction...");
 
-                  const adapter = await getAdapter()
-                  const aptos = getMovement()
+                  const adapter = await getAdapter();
+                  const aptos = getMovement();
                   const transaction = await aptos.transaction.build.simple({
                     sender: userAccount!.address.toString(),
                     data: {
-                      function: '0x1::coin::transfer',
-                      typeArguments: ['0x1::aptos_coin::AptosCoin'],
+                      function: "0x1::coin::transfer",
+                      typeArguments: ["0x1::aptos_coin::AptosCoin"],
                       functionArguments: [
-                        '0xd61ba4b804e961f81e362968f1daf580889346b7cfff0e06f0e0106094b60b5d',
+                        "0xd61ba4b804e961f81e362968f1daf580889346b7cfff0e06f0e0106094b60b5d",
                         1_000_000,
                       ],
                     },
-                  })
+                  });
                   try {
-                    const signedTx = await adapter.signAndSubmitTransaction(transaction)
-                    toast.dismiss(signingToast)
+                    const signedTx = await adapter.signAndSubmitTransaction(
+                      transaction
+                    );
+                    toast.dismiss(signingToast);
 
                     if (signedTx.status !== UserResponseStatus.APPROVED) {
-                      throw new Error('Transaction rejected')
+                      throw new Error("Transaction rejected");
                     }
-                    toast.success('Transaction signed and submitted!', {
+                    toast.success("Transaction signed and submitted!", {
                       action: {
-                        label: 'View on Explorer',
+                        label: "View on Explorer",
                         onClick: () => {
                           window.open(
                             `https://explorer.testnet.suzuka.movementlabs.xyz/#/txn/${signedTx.args.hash}`,
-                            '_blank'
-                          )
+                            "_blank"
+                          );
                         },
                       },
-                    })
+                    });
                   } catch (error) {
-                    toast.error('Transaction rejected')
-                    toast.dismiss(signingToast)
-                    return
+                    toast.error("Transaction rejected");
+                    toast.dismiss(signingToast);
+                    return;
                   }
                 }}
-                name='Sign and Submit'
+                name="Sign and Submit"
               ></ActionStarryButton>
               {/* <ActionStarryButton
                 onClick={async () => {
@@ -161,29 +168,29 @@ const StickyHeader: React.FC = () => {
               <ActionStarryButton
                 onClick={async () => {
                   const signMessage = async () => {
-                    const adapter = await getAdapter()
+                    const adapter = await getAdapter();
                     await adapter.signMessage({
-                      message: 'I love Nightly',
+                      message: "I love Nightly",
                       address: true,
-                      nonce: 'YOLO',
-                    })
-                  }
+                      nonce: "YOLO",
+                    });
+                  };
                   toast.promise(signMessage, {
-                    loading: 'Signing message...',
+                    loading: "Signing message...",
                     success: (_) => {
-                      return `Message signed!`
+                      return `Message signed!`;
                     },
-                    error: 'Operation has been rejected!',
-                  })
+                    error: "Operation has been rejected!",
+                  });
                 }}
-                name='Sign Message'
+                name="Sign Message"
               ></ActionStarryButton>
             </>
           )}
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default StickyHeader
+export default StickyHeader;
