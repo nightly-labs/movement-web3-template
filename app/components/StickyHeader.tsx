bed44db26d4c4607dc27c1c32e9e159c971f7a40
LukassF <szkulkerolf@gmail.com>
2024-09-23 12:20:19 +0200

    fix changing network on requests

--- app/components/StickyHeader.tsx
+++ app/components/StickyHeader.tsx
@@ -1,17 +1,43 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { toast } from "sonner";
 import { getAdapter } from "../misc/adapter";
 import ActionStarryButton from "./ActionStarryButton";
 import StarryButton from "./StarryButton";
-import { AccountInfo, UserResponseStatus } from "@aptos-labs/wallet-standard";
+import {
+  AccountInfo,
+  NetworkInfo,
+  UserResponseStatus,
+} from "@aptos-labs/wallet-standard";
 import { getMovement } from "../misc/movement";
 import { Network } from "@aptos-labs/ts-sdk";
 import { networkMap } from "../misc/utils";
+import { NightlyConnectAptosAdapter } from "@nightlylabs/wallet-selector-aptos";
 
 const StickyHeader: React.FC = () => {
   const [userAccount, setUserAccount] = React.useState<AccountInfo>();
   const [currentNetwork, setCurrentNetwork] = React.useState<string>();
+
+  const changeNetworkBeforeAction = useCallback(
+    async (network: NetworkInfo, adapter: NightlyConnectAptosAdapter) => {
+      if (network.chainId !== 27) {
+        const changeNetworkResponse = await adapter.changeNetwork(
+          networkMap[27]
+        );
+        if (
+          changeNetworkResponse &&
+          changeNetworkResponse.status === UserResponseStatus.APPROVED
+        ) {
+          setCurrentNetwork("Aptos");
+          toast.success("Network changed!");
+        } else {
+          toast.error("User rejected network change");
+          throw new Error("Couldn't change network");
+        }
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     const init = async () => {
@@ -71,11 +97,10 @@ const StickyHeader: React.FC = () => {
             onConnect={async () => {
               const adapter = await getAdapter();
               try {
-                const response = await adapter.connect(undefined, {
-                  chainId: 27,
-                  name: Network.CUSTOM,
-                  url: "https://aptos.testnet.suzuka.movementlabs.xyz/v1",
-                });
+                const response = await adapter.connect(
+                  undefined,
+                  networkMap[27]
+                );
                 if (response.status === UserResponseStatus.APPROVED) {
                   setUserAccount(response.args);
                   const network = await adapter.network();
@@ -96,25 +121,7 @@ const StickyHeader: React.FC = () => {
               try {
                 // Check chainId
                 const chainId = await adapter.network();
-                if (chainId.chainId !== 27) {
-                  // If chainId is different than 4 (movement devnet) change it
-                  const changeNetworkResponse = await adapter.changeNetwork({
-                    chainId: 27,
-                    name: Network.CUSTOM,
-                    url: "https://aptos.testnet.suzuka.movementlabs.xyz/v1",
-                  });
-                  if (
-                    changeNetworkResponse.status === UserResponseStatus.APPROVED
-                  ) {
-                    setCurrentNetwork(
-                      chainId.chainId === 27 ? "Movement" : "Aptos"
-                    );
-                    toast.success("Network changed!");
-                  } else {
-                    toast.error("User rejected network change");
-                    return;
-                  }
-                }
+                await changeNetworkBeforeAction(chainId, adapter);
               } catch (error) {
                 console.log(error);
               }
@@ -138,25 +145,29 @@ const StickyHeader: React.FC = () => {
               <ActionStarryButton
                 onClick={async () => {
                   const signingToast = toast.info("Signing Transaction...");
-
                   const adapter = await getAdapter();
-                  const network = await adapter.network();
-                  const aptos = getMovement(network.chainId);
-                  const transaction = await aptos.transaction.build.simple({
-                    sender: userAccount!.address.toString(),
-                    data: {
-                      function: "0x1::coin::transfer",
-                      typeArguments: ["0x1::aptos_coin::AptosCoin"],
-                      functionArguments: [
-                        "0xd61ba4b804e961f81e362968f1daf580889346b7cfff0e06f0e0106094b60b5d",
-                        1_000_000,
-                      ],
-                    },
-                  });
                   try {
+                    // we have to change the network to movement if we are on aptos
+                    let network = await adapter.network();
+                    // we have to do it here because the flow on mobile is different
+                    await changeNetworkBeforeAction(network, adapter);
+                    network = await adapter.network();
+                    const aptos = getMovement(network.chainId);
+                    const transaction = await aptos.transaction.build.simple({
+                      sender: userAccount!.address.toString(),
+                      data: {
+                        function: "0x1::coin::transfer",
+                        typeArguments: ["0x1::aptos_coin::AptosCoin"],
+                        functionArguments: [
+                          "0xd61ba4b804e961f81e362968f1daf580889346b7cfff0e06f0e0106094b60b5d",
+                          1_000_000,
+                        ],
+                      },
+                    });
                     const signedTx = await adapter.signAndSubmitTransaction(
                       transaction
                     );
+
                     toast.dismiss(signingToast);
 
                     if (signedTx.status !== UserResponseStatus.APPROVED) {
@@ -184,39 +195,44 @@ const StickyHeader: React.FC = () => {
               {/* <ActionStarryButton
                 onClick={async () => {
                   const signTransaction = async () => {
-                    const adapter = await getAdapter()
-                    const aptos = getAptos()
+                    const adapter = await getAdapter();
+                    const network = await adapter.network();
+
+                    const aptos = getMovement(network.chainId);
+
                     const transaction = await aptos.transaction.build.simple({
                       sender: userAccount!.address.toString(),
                       data: {
-                        function: '0x1::coin::transfer',
-                        typeArguments: ['0x1::aptos_coin::AptosCoin'],
+                        function: "0x1::coin::transfer",
+                        typeArguments: ["0x1::aptos_coin::AptosCoin"],
                         functionArguments: [
-                          '0x99881b6cdf90c9edb04e6b5912c236630b55587161dedc1fc05d53f72eec07e8',
+                          "0x99881b6cdf90c9edb04e6b5912c236630b55587161dedc1fc05d53f72eec07e8",
                           100,
                         ],
                       },
-                    })
-                    const signedTx = await adapter.signTransaction(transaction)
+                    });
+                    const signedTx = await adapter.signTransaction(transaction);
                     if (signedTx.status !== UserResponseStatus.APPROVED) {
-                      throw new Error('Transaction rejected')
+                      throw new Error("Transaction rejected");
                     }
-                  }
+                  };
                   toast.promise(signTransaction, {
-                    loading: 'Signing Transaction...',
+                    loading: "Signing Transaction...",
                     success: (_) => {
-                      return `Transaction signed!`
+                      return `Transaction signed!`;
                     },
-                    error: 'Operation has been rejected!',
-                  })
+                    error: "Operation has been rejected!",
+                  });
                 }}
-                name='Sign Transaction'
+                name="Sign Transaction"
               ></ActionStarryButton> */}
 
               <ActionStarryButton
                 onClick={async () => {
                   const signMessage = async () => {
                     const adapter = await getAdapter();
+                    let network = await adapter.network();
+                    await changeNetworkBeforeAction(network, adapter);
                     await adapter.signMessage({
                       message: "I love Nightly",
                       address: true,
@@ -240,7 +256,6 @@ const StickyHeader: React.FC = () => {
                     try {
                       const adapter = await getAdapter();
                       const network = await adapter.network();
-                      console.log(network);
 
                       let changeNetworkResponse;
                       if (network.chainId === 27) {
